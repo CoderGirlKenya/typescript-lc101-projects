3f6bf10147db5d45ce4ebef8155dd0684a2996f0
CoderGirlKenya <kenya78hami@gmail.com>
2021-06-16 21:15:45 -0500

    add ts files

---
+++ studio/Rocket.ts
@@ -0,0 +1,15 @@
+import {Payload} from './Payload';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: string[];
+    astronauts: string[];
+    constructor(name: string, totalCapacityKg: number){
+            this.name = name;
+            this.totalCapacityKg = totalCapacityKg;
+        function sumMass( items: Payload[] ): number{
+            return masskg 
+        }
+    }
+}
